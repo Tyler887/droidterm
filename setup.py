3ae348175ed528afef8156295621c5d475dcc656
Tyler887 <tylermageeshields@gmail.com>
2022-02-25 18:34:37 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+
+    name='Droidterm',
+
+    version='0.1',
+
+    description='Droidterm',
+
+    author='Tyler887',
+
+    author_email="tylermageeshields@gmail.com",
+
+    packages=find_packages(),
+
+    options=options,
+
+    package_data={'testapp': ['*.py']}
+
+)
